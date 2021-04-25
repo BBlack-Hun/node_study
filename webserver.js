b30cc61f34fd2bae2162862f82edf34678fdd872
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-25 23:54:33 +0900

    04_25_study

--- webserver.js
+++ webserver.js
@@ -8,6 +8,7 @@ http
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
   })
+  // 응답이 들어왔을때 실행하는 코드
   .listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
   });
