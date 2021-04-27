cae667092183fa73307c64df87aa4d9f4dfcdde7
BBlack-Hun <h1103j@kangwon.ac.kr>
2021-04-27 22:29:52 +0900

    04_27_study

--- hello.js
+++ hello.js
@@ -1,1 +1,15 @@
-console.log('Hello world!');
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.end('<h1>Hello World!</h1>');
+});
+
+app.get('/login', (req, res) => {
+  console.log('<h1>Login please</h1>');
+});
+
+app.listen(3000, () => {
+  console.log('Connected 3000 port!');
+});
